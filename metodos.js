dfac693aa034f03287246a39bd6557640d9bf653
Vavish <violeta@eterna.digital>
2021-07-01 17:07:10 -0500

    ejemplos

---
+++ metodos.js
@@ -0,0 +1,7 @@
+
+        var ejemplo1 ='Hola Violeta';
+
+        console.log (ejemplo1.toUpperCase());//convierte el texto en mayucsula
+
+        console.log (ejemplo1.toLocaleLowerCase()); //convierte el texto en minuscula
+  
---
+++ sumados.js
@@ -0,0 +1,12 @@
+
+    var suma = function(numero1, numero2){
+        var numero1 = parseInt (document.getElementById("numero1").value);
+        var numero2 = parseInt (document.getElementById("numero2").value);
+        
+        var resultado = numero1 + numero2;
+        return resultado;   
+    
+}
+      
+
+
